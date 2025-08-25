5d06e017d9a7476d037c48f5b1fb92bb8280bb05
Eddy Mageza <magezaeddy@gmail.com>
2025-08-25 20:06:04 +0200

    Add SQL script to insert a single customer row

---
+++ task_5.sql
@@ -0,0 +1,5 @@
+-- task_5.sql
+-- Insert a single row into the customers table
+
+INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
